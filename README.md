f4b455a4bddb300109a23df82bc6d2f6952480c5
spyridouladev <spyridouladev@gmail.com>
2025-07-08 18:08:08 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,23 @@
+# Nuclear_Plants
+
+The purpose of the project is to visualize the location of
+nuclear reactors and their operational status worldwide 
+using the folium library for the map.
+
+## License
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+
+### Data Source
+
+This project uses data from the GeoNuclearData database:
+
+Copyright (c) 2015-2024 Cristian Stoica. 
+The GeoNuclearData database is made available under the Open 
+Database License whose full text can be found at 
+https://opendatacommons.org/licenses/odbl/1.0/.
+It is also included in the project files, see [odbl](odbl-10.txt) file for details.
+
+Any rights in individual contents of the database are licensed 
+under the Database Contents License whose full text can be 
+found at https://opendatacommons.org/licenses/dbcl/1.0/. 
+Repository : [GeoNuclearData GitHub](https://github.com/cristianst85/GeoNuclearData)
---
+++ nuclear_reactors.py
@@ -0,0 +1,350 @@
+import pandas as pd
+from pathlib import Path
+import folium
+from folium.plugins import MarkerCluster
+
+def create_cluster_icon(color):
+    return f"""
+    function(cluster) {{
+        return L.divIcon({{
+            html: `
+                <div style="
+                    background-color: {color};
+                    border-radius: 50%;
+                    box-shadow: 0 0 6px 3px rgba(0, 0, 0, 0);
+                    width: 30px;
+                    height: 30px;
+                    line-height: 30px;
+                    text-align: center;
+                    color: white;
+                    font-weight: bold;
+                    font-size: 16px;
+                    border: 1.5px solid rgba(0, 0, 0, 0);
+                    overflow: visible;
+                ">
+                    <span>` + cluster.getChildCount() + `</span>
+                </div>
+            `,
+            className: 'marker-cluster',
+            iconSize: new L.Point(40, 40)
+        }});
+    }}
+    """
+
+def shutdown(shutdown_df):
+    group = folium.FeatureGroup(name="Shutdown", show=True)
+    marker_cluster = MarkerCluster(
+        icon_create_function=create_cluster_icon("red"),
+        maxClusterRadius=1
+    )
+    shutdown_df.fillna("Not Available", inplace=True)
+    for _, row in shutdown_df.iterrows():
+        shutdown_str = (
+                f"<div style='text-align:center;'><b>{row['Name']}</b></div>"
+                f"<b>Status:</b> Shutdown<br>"
+                f"<b>Reactor Type:</b> {row['ReactorType']}<br>"
+                f"<b>Reactor Model:</b> {row['ReactorModel']}<br>"
+                f"<b>Operational from:</b> {row['OperationalFrom']}<br>"
+                f"<b>Operational to:</b> {row['OperationalTo']}<br>"
+                f"<b>Construction started at:</b> {row['ConstructionStartAt']}<br>"
+                f"<b>Capacity:</b> {row['Capacity']} MWe<br>"
+                f"<b>IAEAId:</b> {int(row['IAEAId'])}<br>"
+                f"<b>Source:</b> {row['Source']}<br>"
+            )        
+        shutdown_popup_html = folium.Popup(folium.Html(shutdown_str, script=True), max_width=300)
+        marker = folium.Marker(
+            location=[row['Latitude'], row['Longitude']],
+            tooltip=row['Name'],
+            popup=shutdown_popup_html,
+            icon=folium.Icon(icon="remove", color="red"),
+        )
+        marker_cluster.add_child(marker)
+    group.add_child(marker_cluster)
+    return group
+
+def operational(operational_df):
+    group = folium.FeatureGroup(name="Operational", show=True)
+    marker_cluster = MarkerCluster(
+        icon_create_function=create_cluster_icon("green"),
+        maxClusterRadius=1
+    ) 
+    operational_df = operational_df.drop(['OperationalFrom','OperationalTo'], axis=1)   
+    operational_df.fillna("Not Available", inplace=True)
+    for _, row in operational_df.iterrows():
+        operational_str =(
+                f"<div style='text-align:center;'><b>{row['Name']}</b></div>"
+                f"<b>Status:</b> Operational<br>"
+                f"<b>Reactor Type:</b> {row['ReactorType']}<br>"
+                f"<b>Reactor Model:</b> {row['ReactorModel']}<br>"
+                f"<b>Construction started at:</b> {row['ConstructionStartAt']}<br>"
+                f"<b>Capacity:</b> {row['Capacity']} MWe<br>"
+                f"<b>IAEAId:</b> {int(row['IAEAId'])}<br>"
+                f"<b>Source:</b> {row['Source']}<br>"
+            )       
+        operational_popup_html = folium.Popup(folium.Html(operational_str, script=True), max_width=300)
+        marker = folium.Marker(
+            location=[row['Latitude'], row['Longitude']],
+            tooltip=row['Name'],
+            popup=operational_popup_html,
+            icon=folium.Icon(icon="ok", color="green"),
+        )
+        marker_cluster.add_child(marker)
+    group.add_child(marker_cluster)
+    return group
+
+def cancelledstruction(cancelledstruction_df):
+    group = folium.FeatureGroup(name="Cancelled Construction", show=True)
+    marker_cluster = MarkerCluster(
+        icon_create_function=create_cluster_icon("orange"),
+        maxClusterRadius=1
+    ) 
+    cancelledstruction_df = cancelledstruction_df.drop(['ReactorModel','ConstructionStartAt','OperationalFrom','OperationalTo'], axis=1)    
+    for _, row in cancelledstruction_df.iterrows():
+        cancelledstruction_str =(
+                f"<div style='text-align:center;'><b>{row['Name']}</b></div>"
+                f"<b>Status:</b> Cancelled Construction<br>"
+                f"<b>Reactor Type:</b> {row['ReactorType']}<br>"
+                f"<b>Capacity:</b> {row['Capacity']} MWe<br>"
+                f"<b>Source:</b> {row['Source']}<br>"
+            )       
+        cancelledstruction_popup_html = folium.Popup(folium.Html(cancelledstruction_str, script=True), max_width=300)
+        marker = folium.Marker(
+            location=[row['Latitude'], row['Longitude']],
+            tooltip=row['Name'],
+            popup=cancelledstruction_popup_html,
+            icon=folium.Icon(icon="remove", color="orange"),
+        )
+        marker_cluster.add_child(marker)
+    group.add_child(marker_cluster)
+    return group
+
+def underconstruction(underconstruction_df):
+    group = folium.FeatureGroup(name="Under Construction", show=True)
+    marker_cluster = MarkerCluster(
+        icon_create_function=create_cluster_icon("orange"),
+        maxClusterRadius=1
+    )
+    underconstruction_df = underconstruction_df.dropna(subset=["Latitude", "Longitude"])
+    underconstruction_df = underconstruction_df.drop(['IAEAId','OperationalFrom','OperationalTo'], axis=1)    
+    underconstruction_df.fillna("Not Available", inplace=True)
+    for _, row in underconstruction_df.iterrows():
+        underconstruction_str =(
+            f"<div style='text-align:center;'><b>{row['Name']}</b></div>"
+            f"<b>Status:</b> Under construction<br>"
+            f"<b>Reactor Type:</b> {row['ReactorType']}<br>"
+            f"<b>Reactor Model:</b> {row['ReactorModel']}<br>"
+            f"<b>Construction started at:</b> {row['ConstructionStartAt']}<br>"
+            f"<b>Capacity:</b> {row['Capacity']} MWe<br>"
+            f"<b>Source:</b> {row['Source']}<br>"
+        )      
+        underconstruction_popup_html = folium.Popup(folium.Html(underconstruction_str, script=True), max_width=300)
+        marker = folium.Marker(
+            location=[row['Latitude'], row['Longitude']],
+            tooltip=row['Name'],
+            popup=underconstruction_popup_html,
+            icon=folium.Icon(icon="wrench", color="orange"),
+        )
+        marker_cluster.add_child(marker)
+    group.add_child(marker_cluster)
+    return group
+
+def planned(planned_df):
+    group = folium.FeatureGroup(name="Planned", show=True)
+    marker_cluster = MarkerCluster(
+        icon_create_function=create_cluster_icon("blue"),
+        maxClusterRadius=1
+    )
+    planned_df = planned_df.drop(['IAEAId','Capacity','ConstructionStartAt','OperationalFrom','OperationalTo'], axis=1)    
+    planned_df.fillna("Not Available", inplace=True)
+    for _, row in planned_df.iterrows():
+        planned_str =(
+            f"<div style='text-align:center;'><b>{row['Name']}</b></div>"
+            f"<b>Status:</b> Planned<br>"
+            f"<b>Reactor Type:</b> {row['ReactorType']}<br>"
+            f"<b>Reactor Model:</b> {row['ReactorModel']}<br>"
+            f"<b>Source:</b> {row['Source']}<br>"
+        )      
+        planned_popup_html = folium.Popup(folium.Html(planned_str, script=True), max_width=300)
+        marker = folium.Marker(
+            location=[row['Latitude'], row['Longitude']],
+            tooltip=row['Name'],
+            popup=planned_popup_html,
+            icon=folium.Icon(icon="time", color="blue"),
+        )
+        marker_cluster.add_child(marker)
+    group.add_child(marker_cluster)
+    return group
+
+def suspended_operation(suspended_operation_df):
+    group = folium.FeatureGroup(name="Suspended Operation", show=True)
+    marker_cluster = MarkerCluster(
+        icon_create_function=create_cluster_icon("darkred"),
+        maxClusterRadius=1
+    )
+    suspended_operation_df = suspended_operation_df.drop(['OperationalTo'], axis=1)    
+    suspended_operation_df.fillna("Not Available", inplace=True)
+    for _, row in suspended_operation_df.iterrows():
+        suspended_operation_str =(
+            f"<div style='text-align:center;'><b>{row['Name']}</b></div>"
+            f"<b>Status:</b> Suspended Operation<br>"
+            f"<b>Reactor Type:</b> {row['ReactorType']}<br>"
+            f"<b>Reactor Model:</b> {row['ReactorModel']}<br>"
+            f"<b>Operational from:</b> {row['OperationalFrom']}<br>"
+            f"<b>Construction started at:</b> {row['ConstructionStartAt']}<br>"
+            f"<b>Capacity:</b> {row['Capacity']} MWe<br>"
+            f"<b>IAEAId:</b> {int(row['IAEAId'])}<br>"
+            f"<b>Source:</b> {row['Source']}<br>"
+        )        
+        suspended_operation_popup_html = folium.Popup(folium.Html(suspended_operation_str, script=True), max_width=300)
+        marker = folium.Marker(
+            location=[row['Latitude'], row['Longitude']],
+            tooltip=row['Name'],
+            popup=suspended_operation_popup_html,
+            icon=folium.Icon(icon="pause", color="darkred"),
+        )
+        marker_cluster.add_child(marker)
+    group.add_child(marker_cluster)
+    return group
+
+def suspended_construction(suspended_construction_df):
+    group = folium.FeatureGroup(name="Suspended Construction", show=True)
+    marker_cluster = MarkerCluster(
+        icon_create_function=create_cluster_icon("orange"),
+        maxClusterRadius=1
+    )
+    suspended_construction_df = suspended_construction_df.drop(['OperationalFrom','OperationalTo','IAEAId'], axis=1)   
+    suspended_construction_df.fillna("Not Available", inplace=True)
+    for _, row in suspended_construction_df.iterrows():
+        suspended_construction_str =(
+            f"<div style='text-align:center;'><b>{row['Name']}</b></div>"
+            f"<b>Status:</b> Suspended Construction<br>"
+            f"<b>Reactor Type:</b> {row['ReactorType']}<br>"
+            f"<b>Reactor Model:</b> {row['ReactorModel']}<br>"
+            f"<b>Construction started at:</b> {row['ConstructionStartAt']}<br>"
+            f"<b>Capacity:</b> {row['Capacity']} MWe<br>"
+            f"<b>Source:</b> {row['Source']}<br>"
+            )        
+        suspended_construction_popup_html = folium.Popup(folium.Html(suspended_construction_str, script=True), max_width=300)
+        marker = folium.Marker(
+            location=[row['Latitude'], row['Longitude']],
+            tooltip=row['Name'],
+            popup=suspended_construction_popup_html,
+            icon=folium.Icon(icon="pause", color="orange"),
+        )
+        marker_cluster.add_child(marker)
+    group.add_child(marker_cluster)
+    return group
+
+def never_comm(never_comm_df):
+    group = folium.FeatureGroup(name="Never Commissioned", show=True)
+    marker_cluster = MarkerCluster(
+        icon_create_function=create_cluster_icon("gray"),
+        maxClusterRadius=1
+    )
+    never_comm_df = never_comm_df.drop(['IAEAId','ConstructionStartAt','OperationalFrom','OperationalTo'], axis=1)    
+    never_comm_df.fillna("Not Available", inplace=True)
+    for _, row in never_comm_df.iterrows():
+        never_comm_str =(
+            f"<div style='text-align:center;'><b>{row['Name']}</b></div>"
+            f"<b>Status:</b> Never Commissioned<br>"
+            f"<b>Reactor Type:</b> {row['ReactorType']}<br>"
+            f"<b>Reactor Model:</b> {row['ReactorModel']}<br>"
+            f"<b>Capacity:</b> {row['Capacity']} MWe<br>"
+            f"<b>Source:</b> {row['Source']}<br>"
+        )        
+        never_comm_popup_html = folium.Popup(folium.Html(never_comm_str, script=True), max_width=300)
+        marker = folium.Marker(
+            location=[row['Latitude'], row['Longitude']],
+            tooltip=row['Name'],
+            popup=never_comm_popup_html,
+            icon=folium.Icon(icon="remove-sign", color="gray"),
+        )
+        marker_cluster.add_child(marker)
+    group.add_child(marker_cluster)
+    return group
+
+def decomm_completed(decomm_completed_df):
+    group = folium.FeatureGroup(name="Decommissioning Completed", show=True)
+    marker_cluster = MarkerCluster(
+        icon_create_function=create_cluster_icon("red"),
+        maxClusterRadius=1
+    )
+    decomm_completed_df = decomm_completed_df.drop(['OperationalFrom'], axis=1)    
+    decomm_completed_df.fillna("Not Available", inplace=True)
+    for _, row in decomm_completed_df.iterrows():
+        decomm_completed_str =(
+            f"<div style='text-align:center;'><b>{row['Name']}</b></div>"
+            f"<b>Status:</b> Decommissioning Completed<br>"
+            f"<b>Reactor Type:</b> {row['ReactorType']}<br>"
+            f"<b>Reactor Model:</b> {row['ReactorModel']}<br>"
+            f"<b>Operational to:</b> {row['OperationalTo']}<br>"
+            f"<b>Construction started at:</b> {row['ConstructionStartAt']}<br>"
+            f"<b>Capacity:</b> {row['Capacity']} MWe<br>"
+            f"<b>IAEAId:</b> {int(row['IAEAId'])}<br>"
+            f"<b>Source:</b> {row['Source']}<br>"
+        )     
+        decomm_completed_popup_html = folium.Popup(folium.Html(decomm_completed_str, script=True), max_width=300)
+        marker = folium.Marker(
+            location=[row['Latitude'], row['Longitude']],
+            tooltip=row['Name'],
+            popup=decomm_completed_popup_html,
+            icon=folium.Icon(icon="check", color="red"),
+        )
+        marker_cluster.add_child(marker)
+    group.add_child(marker_cluster) 
+    return group
+
+def make_map(df):
+    df['Status'] = df['Status'].str.strip().str.title().str.replace(" ", "").str.lower()
+    loc_avg = df['Latitude'].mean(),df['Longitude'].mean()
+    
+    m = folium.Map(
+        location=loc_avg,
+        zoom_start=3,
+        max_bounds=True,
+        no_wrap=True,
+        tiles=None 
+    )
+
+    folium.TileLayer(
+        tiles='https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+        attr='&copy; <a href="https://opendatacommons.org/licenses/odbl/1.0/">GeoNuclearData (ODbL)</a> &copy; <a href="https://carto.com/attributions">CARTO</a> &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',        name='Carto Voyager',
+        min_zoom=3,
+        max_zoom=18,
+    ).add_to(m)
+
+    df = df.drop(['Country','CountryCode','LastUpdatedAt',], axis=1)
+    df = df.dropna(subset=["Latitude", "Longitude"])    
+
+    shutdown_df = df[df['Status'] == 'shutdown'].copy()
+    underconstruction_df = df[df['Status'] == 'underconstruction'].copy()
+    operational_df = df[df['Status'] == 'operational'].copy()
+    cancelledstruction_df = df[df['Status'] == 'cancelledconstruction'].copy()
+    planned_df = df[df['Status'] == 'planned'].copy()
+    suspended_operation_df = df[df['Status'] == 'suspendedoperation'].copy()
+    suspended_construction_df = df[df['Status'] == 'suspendedconstruction'].copy()
+    never_comm_df = df[df['Status'] == 'nevercommissioned'].copy()
+    decomm_completed_df = df[df['Status'] == 'decommissioningcompleted'].copy()
+
+    operational(operational_df).add_to(m)
+    shutdown(shutdown_df).add_to(m)
+    cancelledstruction(cancelledstruction_df).add_to(m)
+    underconstruction(underconstruction_df).add_to(m)
+    planned(planned_df).add_to(m)
+    suspended_operation(suspended_operation_df).add_to(m)
+    suspended_construction(suspended_construction_df).add_to(m)
+    never_comm(never_comm_df).add_to(m)
+    decomm_completed(decomm_completed_df).add_to(m)
+    folium.LayerControl(collapsed=False).add_to(m)
+    return m
+
+def main():
+    file = Path(__file__).parent / "nuclear_power_plants.csv"
+
+    df = pd.read_csv(file)
+
+    m = make_map(df)
+    m.save("index.html")
+
+if __name__ == '__main__':
+    main()
